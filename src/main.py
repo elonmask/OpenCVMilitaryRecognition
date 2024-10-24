8ab885688d636e14ba7a6cb42bf19e30150e1c9b
elonmask <woronowserg@gmail.com>
2024-10-24 02:41:53 +0000

    Fixed crash with YOLOv5

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import cv2
+import torch
 from ultralytics import YOLO
+import os
 
 captureResolution = 1280 # Stream resolution for processing;
 
@@ -26,8 +28,10 @@ if not useSearch:
             # yolov10n.pt - best accuracy;
             model = YOLO("yolov8n.pt")
         else:
-            model_path = "../CustomModels/Vehicles_L/RussianVehicles_L.pt" if militaryModelToUse == 0 else "../CustomModels/Vehicles_S/RussianVehicles_S.pt"
-            model = YOLO(model_path)
+            base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            model_path = os.path.join(base_path, "CustomModels", "Vehicles_L", "RussianVehicles_L.pt") if militaryModelToUse == 0 else os.path.join(base_path, "CustomModels", "Vehicles_S", "RussianVehicles_S.pt")
+            # Load model
+            model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
 else:
         # use model by searching query in YOLO-world dataset for general objects detection;
         model = YOLO("yolov8s-worldv2.pt")
@@ -39,19 +43,25 @@ while True:
         break
     
     results = model(frame)
-    annotated_frame = results[0].plot()
-    
-    inference_time = results[0].speed['inference']
-    fps = 1000 / inference_time
-    text = f'FPS: {fps:.1f}'
+    if civilMode:
+        #print(results)
+        annotated_frame = results[0].plot()
+        inference_time = results[0].speed['inference']
+        fps = 1000 / inference_time
+        text = f'FPS: {fps:.1f}'
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    text_size = cv2.getTextSize(text, font, 1, 2)[0]
-    text_x = annotated_frame.shape[1] - text_size[0] - 10
-    text_y = text_size[1] + 10
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text_size = cv2.getTextSize(text, font, 1, 2)[0]
+        text_x = annotated_frame.shape[1] - text_size[0] - 10
+        text_y = text_size[1] + 10
 
-    cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.imshow("Camera", annotated_frame)
+        cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow("Camera", annotated_frame)
+    else:
+        # only printing results for now
+        # //TODO: implement drawing bounding box like in civilMode;
+        print(results.pandas().xyxy[0])
+        cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) == ord("q"):
         break
